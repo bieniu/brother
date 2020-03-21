d100447163c8953a97ed4c9833930a53f2049d60
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-03-21 21:38:05 +0100

    Fix branch issue

--- brother/__init__.py
+++ brother/__init__.py
@@ -14,7 +14,7 @@ from .const import *
 
 _LOGGER = logging.getLogger(__name__)
 
-REGEX_MODEL_PATTERN = re.compile(r"MODEL=\"(?P<model>[\w\-]+)( series)?\"")
+REGEX_MODEL_PATTERN = re.compile(r"MDL:(?P<model>[\w\-]+)")
 
 
 class Brother:  # pylint:disable=too-many-instance-attributes
@@ -55,13 +55,16 @@ class Brother:  # pylint:disable=too-many-instance-attributes
             self.model = re.search(
                 REGEX_MODEL_PATTERN, raw_data[OIDS[ATTR_MODEL]]
             ).group("model")
+            data[ATTR_MODEL] = self.model
             self.serial = raw_data[OIDS[ATTR_SERIAL]]
+            data[ATTR_SERIAL] = self.serial
         except (TypeError, AttributeError):
             raise UnsupportedModel(
                 "It seems that this printer model is not supported. Sorry."
             )
         try:
             self.firmware = raw_data[OIDS[ATTR_FIRMWARE]]
+            data[ATTR_FIRMWARE] = self.firmware
             code_page = chardet.detect(raw_data[OIDS[ATTR_STATUS]].encode("latin1"))[
                 "encoding"
             ]
