7a415a484fbb8143212f2c09bf4248a53d058813
Maciej Bieniek <maciej.bieniek@gmail.com>
2020-11-25 10:59:32 +0100

    Add pytest-asyncio

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     setup_requires=("pytest-runner"),
-    tests_require=("pytest-cov"),
+    tests_require=("pytest-cov", "pytest-asyncio"),
 )
