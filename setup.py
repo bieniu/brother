671eb90b69e50b7e43feffb0a33b2486e477b32f
Maciej Bieniek <maciej.bieniek@gmail.com>
2021-02-24 11:23:16 +0100

    Move requires to files

--- setup.py
+++ setup.py
@@ -10,15 +10,15 @@ setup(
         printers via SNMP.",
     include_package_data=True,
     url="https://github.com/bieniu/brother",
-    license="Apache 2",
+    license="Apache License 2.0",
     packages=["brother"],
     python_requires=">=3.6",
-    install_requires=["pysnmp"],
+    install_requires=list(val.strip() for val in open("requirements.txt")),
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Operating System :: OS Independent",
     ],
     setup_requires=("pytest-runner"),
-    tests_require=("pytest-cov", "pytest-asyncio", "pytest-error-for-skips"),
+    tests_require=list(val.strip() for val in open("requirements-test.txt")),
 )
