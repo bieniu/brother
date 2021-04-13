74458394f940050ad9e58ce63b61c290ab73ae1d
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-13 09:22:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="brother",
     version="0.2.2",
     author="Maciej Bieniek",
-    author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting data from Brother laser and inkjet \
         printers via SNMP.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/brother",
     license="Apache-2.0 License",
