17dfcca2b2a4296741ca4bd19453a7f1c31a4345
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-22 11:16:41 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="brother",
-    version="0.2.2",
+    version="1.0.0",
     author="Maciej Bieniek",
     description="Python wrapper for getting data from Brother laser and inkjet \
         printers via SNMP.",
