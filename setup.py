f6118753e105a2bdb13e114422883c3322416059
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-10-02 11:43:29 +0200

    Change version to 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements-test.txt", "r", encoding="utf-8") as file:
 
 setup(
     name="brother",
-    version="2.0.0",
+    version="1.1.0",
     author="Maciej Bieniek",
     description="Python wrapper for getting data from Brother laser and inkjet \
         printers via SNMP.",
