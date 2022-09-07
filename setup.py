b99ad12032b44138a935a81e7bdce800424ba2b7
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-09-07 14:13:00 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 PROJECT_DIR = Path(__file__).parent.resolve()
 README_FILE = PROJECT_DIR / "README.md"
-VERSION = "1.2.3"
+VERSION = "2.0.0"
 
 setup(
     name="brother",
